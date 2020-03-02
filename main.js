14dfd798f6d7c01a24ee8130fd4239b0895ef361
Matthew Berry <41410700+Infraction71@users.noreply.github.com>
2020-03-02 10:35:38 -0600

    Challenge complete
    Well that was fun

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 function getLength(){
-    //alert("Method called");
-        const MESSAGE = "The word length is: "; 
-      //  alert('We are about to get the value');
-        let input = document.getElementById("wordIn").value;
-        
-        let wordLen = input.length;
-
-      //  alert('We Got the value: '+ speedInMPH);
-      //  alert('We did a conversion');
-        document.getElementById("wordLenOut").innerHTML = MESSAGE + wordLen;
+    
+        const MESSAGE = "The word length is: "; //For printing later
+        let wordIN = document.getElementById("wordIn").value;  //Getting value 
+        let wordLen = wordIN.length //Getting the length
+        document.getElementById("wordLenOut").innerHTML = MESSAGE + wordLen; //printing the length :) 
     }
     
   
